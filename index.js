b80f42e751977a3767dad114810c6c1e71fb2343
mosh9098 <mosh9098@gmail.com>
2024-03-21 12:55:35 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply);
+
+let random = Math.floor(Math.random() * 10) + 1;
+console.log();
+
+let num3 = 16;
+let num4 = 4;
+let mod = num3 / num4;
+console.log(mod%3);
+
+let num5 = 10;
+let num6 = 20;
+let num7 = 5;
+let num8 = 10;
+let max = Math.max(num5, num6, num7, num8);
+
+console.log(max); 
